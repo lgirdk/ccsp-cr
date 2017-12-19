1f1b9470f01606f232ec1412f5c6c93943dd4fc5
Lakshminarayanan Shenbagaraj <Lakshminarayanan_Shenbagaraj@comcast.com>
2017-12-19 16:51:53 +0530

    RDKB-12488 RDKB does not support the option to override the debug.ini file
    Reason for change: Added Support for overriding debug.ini file location
    Test Procedure: 1. Take a copy of /etc/debug.ini to /nvram/debug.ini.
    	2. Configure the debug.ini present in /nvram.
    	3. Reboot the box/module.
    	4. It should take the debug option from /nvram/debug.ini instead of /etc/debug.ini
    Risks: None
    Signed-off-by: Gokulakrishnan R <gokulakrishnan.r01@infosys.com>
    
    (cherry picked from commit 4d9e56d7795679dd5f55a2b53bf07f81e8ad826d)
    (cherry picked from commit 1853a07cc82709bcec0923f1d0b45a9c0d2e37e5)
    (cherry picked from commit 4a0397c254e3abf1f089fbe03bd5ca69b8e7bf22)
    (cherry picked from commit d895efa4cabfa24ff2482463d0cfac8b127ad8e9)
    (cherry picked from commit 01b1052bdcdb4eb08372b7f65bbfcacf1f130d30)
    
    Change-Id: I9508feaa1070a43900c30c6f3b3d188a0346e5ee

--- source/CrSsp/ssp_main.c
+++ source/CrSsp/ssp_main.c
@@ -434,9 +434,9 @@ int main(int argc, char* argv[])
 #endif
 
     pComponentName = CCSP_DBUS_INTERFACE_CR;
-	#ifdef FEATURE_SUPPORT_RDKLOG
-	rdk_logger_init(DEBUG_INI_NAME);
-	#endif
+#ifdef FEATURE_SUPPORT_RDKLOG
+    RDK_LOGGER_INIT();
+#endif
 
 #if defined(_DEBUG) || defined(_COSA_SIM_)
     AnscSetTraceLevel(CCSP_TRACE_LEVEL_INFO);
